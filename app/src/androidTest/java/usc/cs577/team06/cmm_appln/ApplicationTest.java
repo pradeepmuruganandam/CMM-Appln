38b50ffc5d3e4067b460920195de09ee64a447a3
Pradeep Muruganandam <pmurugan.usc.edu>
2015-11-03 00:24:27 -0800

    Initial commit

---
+++ app/src/androidTest/java/usc/cs577/team06/cmm_appln/ApplicationTest.java
@@ -0,0 +1,13 @@
+package usc.cs577.team06.cmm_appln;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
