96648cef288189f9d0acdb1fd4c8907d63428ad1
Pradeep Muruganandam <pmurugan.usc.edu>
2015-11-04 16:52:28 -0800

    Added a Choose Project page and Intents

--- app/src/main/java/usc/cs577/team06/cmm_appln/ProjectChooseActivity.java
+++ app/src/main/java/usc/cs577/team06/cmm_appln/ProjectChooseActivity.java
@@ -25,15 +25,6 @@ public class ProjectChooseActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         ListView lv = (ListView)findViewById(R.id.ProjectList);
         TextView emptyText = (TextView)findViewById(android.R.id.empty);
         lv.setEmptyView(emptyText);
